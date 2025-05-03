143379b1ce7eb6c26a3f0bb3141a0bb73d3523df
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-05-03 17:08:41 +0300

    create:Assignment-02

---
+++ Assignment-02/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            Console.WriteLine($"Equation: {x} + {y} = {x + y:x}");
+        }
+    }
+}
