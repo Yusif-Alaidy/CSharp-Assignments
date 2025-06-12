524e23d43202ca2b571dc3b11db756ab72ba6bf2
Yusif <110829739+Yusif-Alaidy@users.noreply.github.com>
2025-06-12 23:28:54 +0300

    Build: search task 05

---
+++ Search1-05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml;
+
+namespace Search1_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = new();
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("\t╔==============================╗\n"+
+                              "\t║   Enter Your Number Please   ║\n"+
+                              "\t╚==============================╝\n");
+                Console.Write("\t==>: ");
+                int unique = Convert.ToInt32(Console.ReadLine());
+                if (number.Contains(unique))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    throw new Exception("This number already exist !");
+                }
+                number.Add(unique);
+                
+            } while (true);
+        }
+    }
+}
